a6a07992b33c3be8776ac339092cabdf07b87b8c
Bas Tuijnman <bas.tuijnman@24i.com>
2015-11-25 09:55:36 +0100

    added test for creating an attachment

--- tests/unit/slack/attachment.spec.js
+++ tests/unit/slack/attachment.spec.js
@@ -1,6 +1,14 @@
 describe('Slack attachment test suite', function () {
     'use strict';
 
+    let slackAttachment = require('../../../src/slack/attachment');
 
+    it('should be able to create an attachment', function () {
+        let message = 'A **formatted** message',
+            attachment = slackAttachment(message);
+
+        expect(attachment).not.toBeUndefined();
+        expect(attachment.getText()).toBe(message);
+    });
 
 });
