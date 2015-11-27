b5daf418904cdddd9695e53b7f96fba785059956
Bas Tuijnman <bas.tuijnman@24i.com>
2015-11-27 13:05:43 +0100

    added hook methods

--- src/hook.js
+++ src/hook.js
@@ -1,7 +1,35 @@
 'use strict';
 
+let http = require('http'),
+    url = require('url'),
+    message = require('./slack/message'),
+    attachment = require('./slack/attachment');
+
 module.exports = {
 
+    created: function (issue) {
+        let msg = message(issue.key);
+        this.send(msg);
+    },
 
+    send: function (msg) {
+        let slackUrl = url.parse(process.env.SLACK_HOOK_URL),
+            request;
+
+        request = http.request({
+            method: 'POST',
+            protocol: slackUrl.protocol,
+            host: slackUrl.host,
+            path: slackUrl.path,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        request.write(msg.toJSON());
+        request.end();
+
+        return request;
+    }
 
 };
--- tests/unit/hook.spec.js
+++ tests/unit/hook.spec.js
@@ -1,7 +1,35 @@
 describe('receiver test suite', function () {
     'use strict';
 
-    let hook = require('../../src/hook');
+    let http = require('http'),
+        hook = require('../../src/hook'),
+        message = require('../../src/slack/message'),
+        response = require('../responses/jira/issue.json'),
+        issue = require('../../src/jira/issue')(response);
 
+    beforeEach(function () {
+        spyOn(http, 'request').and.callThrough();
+    });
+
+    it('should be able to send a message', function () {
+        let msg = message('basic message'),
+            req = hook.send(msg);
+
+        expect(http.request).toHaveBeenCalledWith({
+            method: 'POST',
+            protocol: 'http:',
+            host: 'localhost',
+            path: '/slack-hook',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    });
+
+    it('should be able to send an issue created message', function () {
+        spyOn(hook, 'send');
+        hook.created(issue);
+        expect(hook.send).toHaveBeenCalled();
+    });
 
 });
