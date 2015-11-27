6e73baed1c0f813d30db669004054570da5768af
Bas Tuijnman <bas.tuijnman@24i.com>
2015-11-27 13:15:07 +0100

    added middleware to server to parse hooks

--- index.js
+++ index.js
@@ -2,13 +2,38 @@
     'use strict';
 
     let express = require('express'),
+        listener = require('./src/listener'),
+        hook = require('./src/hook'),
         app,
         server;
 
     app = express();
 
+    // Little bit of middleware to get the raw request body
+    app.use(function (req, res) {
+        let data = '';
+
+        req.setEncoding('utf8');
+        req.on('data', function (chunk) {
+            data += chunk;
+        });
+
+        req.on('end', function () {
+            req.body = data;
+            next();
+        });
+    })
+
+    // Propagate request bodies through to the listener
+    app.post('/', function (req, res, next) {
+        listener.receive(req.body);
+    });
+
     // Run our webhook server
     server = app.listen(process.env.EXPRESS_PORT || 3000, function () {
+
+        listener.on('issue.created', hook.created.bind(hook));
+
         process.stdout.write('Server running!\n');
     });
 
