7747655e608c383fa9a8778505fc68ec71ef1807
Bas Tuijnman <bas.tuijnman@24i.com>
2015-11-27 11:29:39 +0100

    added JSDoc

--- src/listener.js
+++ src/listener.js
@@ -8,10 +8,22 @@ let EventEmitter = require('events'),
 
 listener = new EventEmitter();
 
+/**
+ * parses the hook type for usage as event name
+ *
+ * @param  {string} hookType The hook type from the response
+ * @return {string}          The new event name
+ */
 parseType = function (hookType) {
     return hookType.replace(/jira\:([a-z]+)\_([a-z]+)/, '$1.$2');
 };
 
+/**
+ * receive the response and emit the corresponding event with the
+ * issue
+ *
+ * @param  {string} response The response from the webhook
+ */
 listener.receive = function (response) {
     let evnt;
 
